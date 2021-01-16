70f1c56d163a7f31ba32de7f8b584635b46ccef2
Marcus Butler <marcus@pop.local.asterick.co>
2021-01-15 18:57:31 -0600

    change postgresdn type to AnyUrl for sqlite test

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ import logging
 import os
 from functools import lru_cache
 
-from pydantic import BaseSettings, PostgresDsn
+from pydantic import AnyUrl, BaseSettings
 
 log = logging.getLogger("uvicorn")
 
@@ -13,7 +13,7 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
     environment: str = os.getenv("ENVIRONMENT", "dev")
     testing: bool = os.getenv("TESTING", False)
-    database_url: PostgresDsn = os.environ.get("DATABASE_URL")
+    database_url: AnyUrl = os.environ.get("DATABASE_URL")
 
 
 @lru_cache
